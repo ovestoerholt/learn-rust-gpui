ecadd40003a43e5fe169e57382f1d8a159e42e5f
Ove Stoerholt <ove@inforte.no>
2025-11-24 08:51:05 +0100

    Working color mixer

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,115 @@
 use gpui::{
-    App, Application, Bounds, Context, SharedString, Window, WindowBounds, WindowOptions, div,
-    prelude::*, px, rgb, size,
+    App, Application, Bounds, Context, Window, WindowBounds, WindowOptions, div, prelude::*, px,
+    rgb, size,
 };
 
-struct HelloWorld {
-    text: SharedString,
+struct ColorMixer {
+    red: f32,
+    green: f32,
+    blue: f32,
 }
 
-impl Render for HelloWorld {
-    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+impl ColorMixer {
+    fn new(_cx: &mut Context<Self>) -> Self {
+        Self {
+            red: 0.5,
+            green: 0.5,
+            blue: 0.5,
+        }
+    }
+
+    fn set_red(&mut self, value: f32, cx: &mut Context<Self>) {
+        self.red = value;
+        cx.notify();
+    }
+
+    fn set_green(&mut self, value: f32, cx: &mut Context<Self>) {
+        self.green = value;
+        cx.notify();
+    }
+
+    fn set_blue(&mut self, value: f32, cx: &mut Context<Self>) {
+        self.blue = value;
+        cx.notify();
+    }
+
+    fn render_channel_control(
+        &self,
+        label: &'static str,
+        value: f32,
+        setter: impl Fn(&mut Self, f32, &mut Context<Self>) + 'static + Copy,
+        cx: &mut Context<Self>,
+    ) -> impl IntoElement {
         div()
             .flex()
             .flex_col()
-            .gap_3()
-            .bg(rgb(0x505050))
-            .size(px(500.0))
-            .justify_center()
-            .items_center()
-            .shadow_lg()
-            .border_1()
-            .border_color(rgb(0x0000ff))
-            .text_xl()
+            .gap_1()
+            .child(label)
+            .child(div().flex().gap_1().children((0..20).map(|i| {
+                let v = (i as f32) / 19.0;
+                let active = v <= value;
+                div()
+                    .id((label, i as usize))
+                    .w_4()
+                    .h_6()
+                    .bg(if active { rgb(0xffffff) } else { rgb(0x505050) })
+                    .cursor_pointer()
+                    .on_click(cx.listener(move |this, _, _, cx| {
+                        setter(this, v, cx);
+                    }))
+            })))
+    }
+}
+
+impl Render for ColorMixer {
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        let color = rgb(((self.red * 255.0) as u32) << 16
+            | ((self.green * 255.0) as u32) << 8
+            | ((self.blue * 255.0) as u32));
+
+        div()
+            .flex()
+            .flex_col()
+            .size_full()
+            .bg(rgb(0x202020))
             .text_color(rgb(0xffffff))
-            .child(format!("Hello, {}!", &self.text))
             .child(
                 div()
                     .flex()
-                    .gap_2()
-                    .child(
-                        div()
-                            .size_8()
-                            .bg(gpui::red())
-                            .border_1()
-                            .border_dashed()
-                            .rounded_md()
-                            .border_color(gpui::white()),
-                    )
-                    .child(
-                        div()
-                            .size_8()
-                            .bg(gpui::green())
-                            .border_1()
-                            .border_dashed()
-                            .rounded_md()
-                            .border_color(gpui::white()),
-                    )
-                    .child(
-                        div()
-                            .size_8()
-                            .bg(gpui::blue())
-                            .border_1()
-                            .border_dashed()
-                            .rounded_md()
-                            .border_color(gpui::white()),
-                    )
-                    .child(
-                        div()
-                            .size_8()
-                            .bg(gpui::yellow())
-                            .border_1()
-                            .border_dashed()
-                            .rounded_md()
-                            .border_color(gpui::white()),
-                    )
-                    .child(
-                        div()
-                            .size_8()
-                            .bg(gpui::black())
-                            .border_1()
-                            .border_dashed()
-                            .rounded_md()
-                            .rounded_md()
-                            .border_color(gpui::white()),
-                    )
-                    .child(
-                        div()
-                            .size_8()
-                            .bg(gpui::white())
-                            .border_1()
-                            .border_dashed()
-                            .rounded_md()
-                            .border_color(gpui::black()),
-                    ),
+                    .size_full()
+                    .bg(color)
+                    .justify_center()
+                    .items_center()
+                    .child(format!(
+                        "R: {:.2} G: {:.2} B: {:.2}",
+                        self.red, self.green, self.blue
+                    )),
+            )
+            .child(
+                div()
+                    .flex()
+                    .flex_col()
+                    .p_4()
+                    .gap_4()
+                    .bg(rgb(0x303030))
+                    .child(self.render_channel_control(
+                        "Red",
+                        self.red,
+                        |this, val, cx| this.set_red(val, cx),
+                        cx,
+                    ))
+                    .child(self.render_channel_control(
+                        "Green",
+                        self.green,
+                        |this, val, cx| this.set_green(val, cx),
+                        cx,
+                    ))
+                    .child(self.render_channel_control(
+                        "Blue",
+                        self.blue,
+                        |this, val, cx| this.set_blue(val, cx),
+                        cx,
+                    )),
             )
     }
 }
@@ -94,11 +122,7 @@ fn main() {
                 window_bounds: Some(WindowBounds::Windowed(bounds)),
                 ..Default::default()
             },
-            |_, cx| {
-                cx.new(|_| HelloWorld {
-                    text: "World".into(),
-                })
-            },
+            |_, cx| cx.new(|cx| ColorMixer::new(cx)),
         )
         .unwrap();
         cx.activate(true);
